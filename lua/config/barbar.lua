ebde7e0cc4a5b803be19edd18a0172711ddfa7bc
lixinnian <369230782@qq.com>
2022-07-07 21:17:50 +0800

    nvimconfig 0.1

--- lua/config/barbar.lua
+++ lua/config/barbar.lua
@@ -3,14 +3,14 @@
 require("bufferline").setup({})
 
 -- nvimtree插件集成
-vim.api.nvim_create_autocmd("BufWinEnter", {
-	pattern = "*",
-	callback = function()
-		if vim.bo.filetype == "NvimTree" then
-			require("bufferline.state").set_offset(31, "FileTree")
-		end
-	end,
-})
+-- vim.api.nvim_create_autocmd("BufWinEnter", {
+-- 	pattern = "*",
+-- 	callback = function()
+-- 		if vim.bo.filetype == "NvimTree" then
+-- 			require("bufferline.state").set_offset(31, "FileTree")
+-- 		end
+-- 	end,
+-- })
 
 vim.api.nvim_create_autocmd("BufWinLeave", {
 	pattern = "*",
