e769af6376e1c305f4aa35424c8834db00d40664
lixinnian <369230782@qq.com>
2022-07-08 10:58:52 +0800

    nvimconfig 0.1

--- lua/config/gitsigns.lua
+++ lua/config/gitsigns.lua
@@ -24,14 +24,22 @@ require('gitsigns').setup{
     end, {expr=true})
 
     -- Actions
+        -- 提交修改
     map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+        -- 撤销修改
     map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+        -- 提交buffer内所有修改
     map('n', '<leader>hS', gs.stage_buffer)
     map('n', '<leader>hu', gs.undo_stage_hunk)
+        -- 撤销buffer内所有修改
     map('n', '<leader>hR', gs.reset_buffer)
+        -- 浮窗预览修改
     map('n', '<leader>hp', gs.preview_hunk)
+        -- 浮窗对比差异
     map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+        -- 开/关虚拟文本提示
     map('n', '<leader>tb', gs.toggle_current_line_blame)
+        -- 左右窗口差异对比
     map('n', '<leader>hd', gs.diffthis)
     map('n', '<leader>hD', function() gs.diffthis('~') end)
     map('n', '<leader>td', gs.toggle_deleted)
